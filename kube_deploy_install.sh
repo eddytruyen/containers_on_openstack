c08cab8bdd58cbc042ee7215e47d3a9c671bdc12
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2016-06-13 14:45:59 +0000

    kube-deploy

---
+++ kube_deploy_install.sh
@@ -0,0 +1,7 @@
+#cd containers_on_openstack
+#git pull
+#cd ..
+#git clone https://github.com/kubernetes/kube-deploy.git
+cd containers_on_openstack
+cp worker.sh master.sh ~/kube-deploy/docker-multinode
+./clean-reinstall-docker-pre-reboot.sh
