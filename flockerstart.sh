69386fba2898a69e040d90ff17c523395b2559e3
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2016-06-21 13:32:56 +0000

    flockerstart

---
+++ flockerstart.sh
@@ -0,0 +1,7 @@
+cat <<EOF > /etc/init/flocker-control.override
+start on runlevel [2345]
+stop on runlevel [016]
+EOF
+echo 'flocker-control-api	4523/tcp			# Flocker Control API port' >> /etc/services
+echo 'flocker-control-agent	4524/tcp			# Flocker Control Agent port' >> /etc/services
+service flocker-control start
