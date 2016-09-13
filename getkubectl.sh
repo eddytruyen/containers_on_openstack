434450d07329e56cf7e63fabc88d9ee7a2930c6e
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2016-09-13 15:20:51 +0000

    get kubectl update

--- getkubectl.sh
+++ getkubectl.sh
@@ -1,2 +1,2 @@
-curl -sSL https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kubectl > /usr/local/bin/kubectl
+curl -sSL https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl > /usr/local/bin/kubectl
 chmod +x /usr/local/bin/kubectl
