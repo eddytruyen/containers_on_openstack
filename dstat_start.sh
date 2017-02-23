c582b1b6245996c8103c025be10960dba9858d4a
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2017-02-23 14:22:13 +0000

    dstat

---
+++ dstat_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+l=${#1}
+if (( l==0 )); then 
+  echo no argument 
+  exit
+fi
+files=`ls`
+if [[ $files =~ .*$1.* ]] ; then
+  echo file $1 already exists
+  exit
+fi
+
+dstat -t -cp  -mgs --vm -r -d  -D total --fs --lock  -n --socket  --ipc  -y --noheaders --output $1 --noupdate 5  > /dev/null
