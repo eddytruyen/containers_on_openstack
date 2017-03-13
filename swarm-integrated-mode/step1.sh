64e31455c5b311715a25d251db2d1f3789033fe7
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2017-03-13 21:18:46 +0000

    step1

--- swarm-integrated-mode/step1.sh
+++ swarm-integrated-mode/step1.sh
@@ -1,4 +1,5 @@
-apt-get update && apt-get install -y apt-transport-https
+apt-get update 
+apt-get install  apt-transport-https -y
 apt-get update
 # Install docker if you don't have it already.
 apt-get install -y docker.io
