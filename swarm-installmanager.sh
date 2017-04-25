c8122f224cfb9251bd59bee7f6161c474c004e54
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2017-04-25 09:58:00 +0000

    swarm 1.1.3

--- swarm-installmanager.sh
+++ swarm-installmanager.sh
@@ -3,4 +3,4 @@ sudo rm nohup.out
 sudo nohup docker daemon -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock &
 sleep 5
 docker run -d -p 8500:8500 --name=consul gliderlabs/consul:0.6  agent -server -bootstrap -data-dir=/data/consul -client=0.0.0.0
-docker run -d -p ${MASTER_IP}:4000:4000 swarm manage -H :4000 --replication --advertise ${MASTER_IP}:4000 consul://${MASTER_IP}:8500
+docker run -d -p ${MASTER_IP}:4000:4000 swarm:1.1.3 manage -H :4000 --replication --advertise ${MASTER_IP}:4000 consul://${MASTER_IP}:8500
--- swarm-installworker.sh
+++ swarm-installworker.sh
@@ -1,5 +1,5 @@
 sudo service docker stop
 sudo nohup docker daemon --label NodeIP=`/sbin/ip addr show eth0 | awk -F"[ /]+" '/inet / {print $3}'` -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock &
 sleep 5;
-docker run -d swarm join --advertise=`/sbin/ip addr show eth0 | awk -F"[ /]+" '/inet / {print $3}'`:2375 consul://${CONSUL_IP}:8500
+docker run -d swarm:1.1.3 join --advertise=`/sbin/ip addr show eth0 | awk -F"[ /]+" '/inet / {print $3}'`:2375 consul://${CONSUL_IP}:8500
 
