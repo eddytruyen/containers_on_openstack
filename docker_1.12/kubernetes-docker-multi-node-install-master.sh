279d4ed388b7fb310318e4f88273f4811183995c
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2017-04-28 15:10:35 +0000

    kubectl install

--- docker_1.12/kubernetes-docker-multi-node-install-master.sh
+++ docker_1.12/kubernetes-docker-multi-node-install-master.sh
@@ -3,3 +3,5 @@ source ./kubernetes_env.sh
 ./clean-reinstall-docker-post-reboot.sh
 sudo iptables -P FORWARD ACCEPT
 $KUBE_DEPLOY_HOMEDIR/kube-deploy/docker-multinode/master.sh
+curl -sSL https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kubectl > /usr/local/bin/kubectl
+chmod +x /usr/local/bin/kubectl
