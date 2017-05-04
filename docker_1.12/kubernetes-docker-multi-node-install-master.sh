0ad02bf63a6261acdd8d9dd88173be7efdb6de74
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2017-05-04 23:50:17 +0000

    eddy

--- docker_1.12/kubernetes-docker-multi-node-install-master.sh
+++ docker_1.12/kubernetes-docker-multi-node-install-master.sh
@@ -1,7 +1,9 @@
+
 #execute as root
 source ./kubernetes_env.sh
 ./clean-reinstall-docker-post-reboot.sh
-sudo iptables -P FORWARD ACCEPT
 $KUBE_DEPLOY_HOMEDIR/kube-deploy/docker-multinode/master.sh
 curl -sSL https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kubectl > /usr/local/bin/kubectl
 chmod +x /usr/local/bin/kubectl
+iptables -P FORWARD ACCEPT
+
--- docker_1.12/kubernetes-docker-multi-node-install-worker.sh
+++ docker_1.12/kubernetes-docker-multi-node-install-worker.sh
@@ -1,5 +1,6 @@
 #execute as root
 source ./kubernetes_env.sh
 ./clean-reinstall-docker-post-reboot.sh
-sudo iptables -P FORWARD ACCEPT
 $KUBE_DEPLOY_HOMEDIR/kube-deploy/docker-multinode/worker.sh
+iptables -P FORWARD ACCEPT
+
