e70acd1892848adb49fef4925718e023855b756e
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2017-05-26 19:43:49 +0000

    update

--- kubeadm/step1.sh
+++ kubeadm/step1.sh
@@ -7,5 +7,5 @@ apt-get update
 ./dockerinstall
 apt-get update
 # Install docker if you don't have it already.
-apt-get install kubeadm=1.5.7-00 kubelet=1.6.1-00 kubectl kubernetes-cni
+apt-get install -y kubelet kubeadm kubectl kubernetes-cni
 ./pinversions.sh
