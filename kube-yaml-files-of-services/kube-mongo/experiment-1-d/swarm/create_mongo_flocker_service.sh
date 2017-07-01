4b050d41bef62086cd552ed27a52dee2b973506c
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2017-07-01 15:42:07 +0000

    experiment 1-d swarm

--- kube-yaml-files-of-services/kube-mongo/experiment-1-d/swarm/create_mongo_flocker_service.sh
+++ kube-yaml-files-of-services/kube-mongo/experiment-1-d/swarm/create_mongo_flocker_service.sh
@@ -1,2 +1,2 @@
-docker service create --network overlay --constraint node.hostname==swarm-worker-near-ycsb --mount type=volume,src=mongo,dst=/data/db,volume-driver=flocker --name mongo-1 --replicas=1 --publish mode=host,target=27017,published=27017,protocol=tcp  mongo:3.2.12 --storageEngine wiredTiger --logappend --logpath /var/log/mongodb/mongod.log --bind_ip 0.0.0.0
+docker service create --constraint node.hostname==swarm-worker --mount type=volume,src=mongo,dst=/data/db,volume-driver=flocker --name mongo-1 --replicas=1 --publish mode=host,target=27017,published=27017,protocol=tcp  mongo:3.2.4 --storageEngine wiredTiger --logappend --logpath /var/log/mongodb/mongod.log --bind_ip 0.0.0.0
 
