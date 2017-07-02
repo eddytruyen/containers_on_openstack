feedbd4ddf88ef8d920e0d55b8bccf62076351f3
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2017-07-02 17:33:52 +0000

    swarm update

--- kube-yaml-files-of-services/kube-mongo/experiment-2/swarm/create_mongo_service_node-port.sh
+++ kube-yaml-files-of-services/kube-mongo/experiment-2/swarm/create_mongo_service_node-port.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker service create --network overlay --constraint node.hostname==swarm-worker-$1 --name mongo-service-$1 --mount target=/data/db,source=mongodb-$1,type=volume,volume-driver=local -p 27017 mongo:3.2.4 --replSet rs0 --logappend --logpath /var/log/mongodb/mongod.log --bind_ip 0.0.0.0
+docker service create --network overlay --constraint node.hostname==docker-swarm-worker-$1 --name mongo-service-$1 --mount target=/data/db,source=mongodb-$1,type=volume,volume-driver=local -p 27017 mongo:3.2.4 --replSet rs0 --logappend --logpath /var/log/mongodb/mongod.log --bind_ip 0.0.0.0
--- kube-yaml-files-of-services/kube-mongo/experiment-2/swarm/create_ycsb.sh
+++ kube-yaml-files-of-services/kube-mongo/experiment-2/swarm/create_ycsb.sh
@@ -1,1 +1,1 @@
-docker service create --constraint node.hostname==ycsb-4 --network overlay --mount target=/data/,source=ycsb,type=volume,volume-driver=local --name ycsb decomads/ycsb-middleware2017 start.sh
+docker service create --constraint node.hostname==yscb-3 --network overlay --mount target=/data/,source=ycsb,type=volume,volume-driver=local --name ycsb decomads/ycsb-middleware2017 start.sh
