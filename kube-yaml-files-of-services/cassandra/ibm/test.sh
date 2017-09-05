9399309c7fdfb01c4aa6cdc5763e7cd9eb8a0560
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2017-09-05 08:55:13 +0000

    2-cassandra ServiceIP and kubeadm-weave

---
+++ kube-yaml-files-of-services/cassandra/ibm/test.sh
@@ -0,0 +1,1 @@
+echo 100x100 calls >> run1; date >> run1; for a in `seq 100`; do for i in 32041 31856 32760 31979 32366 30815 32284 31497 30860 31950; do for j in `seq 10`; do curl -w "@curl-format.txt" -o /dev/null 172.17.13.45:$i >> run1.csv; done; done; done; date >> run1 
---
+++ kubeadm/install-hostmapper.sh
@@ -0,0 +1,23 @@
+cd /tmp && mkdir cni-plugins && cd cni-plugins && \
+    wget https://github.com/containernetworking/plugins/releases/download/v0.6.0-rc1/cni-plugins-amd64-v0.6.0-rc1.tgz && \
+    tar zxfv cni-plugins-amd64-v0.6.0-rc1.tgz
+sudo cp /tmp/cni-plugins/portmap /opt/cni/bin/
+sudo sh -c 'cat >/etc/cni/net.d/10-mynet.conflist <<-EOF
+{
+    "cniVersion": "0.3.0",
+    "name": "mynet",
+      "plugins": [
+        {
+            "name": "weave",
+            "type": "weave-net",
+            "hairpinMode": true
+        },
+        {
+            "type": "portmap",
+            "capabilities": {"portMappings": true},
+            "snat": true
+        }
+    ]
+}
+EOF'
+sudo rm /etc/cni/net.d/10-weave.conf
