e4183ec33b02abf7c5c9b9fdd8ef2240283932c9
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2017-09-05 17:10:14 +0000

    Revert "update"
    This reverts commit f0b9b0627bdd2dadc1528ce824bf619b59ab1ceb.

--- mongo_native/start_mongod.sh
+++
@@ -1,2 +0,0 @@
-mongod --logappend --logpath /var/log/mongodb/mongod.log --bind_ip 0.0.0.0
-
--- mongo_native/start_mongod_rs.sh
+++
@@ -1,1 +0,0 @@
-mongod --replSet rs0 --logappend --logpath /var/log/mongodb/mongod.log --bind_ip 0.0.0.0
