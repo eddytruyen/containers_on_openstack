59210e15b1a288b412c0f2596ca10bb8da78ecc4
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2017-09-09 13:57:40 +0000

    update

--- kube-yaml-files-of-services/kube-mongo/experiment-2/docker_only/create_mongo_instance.sh
+++ kube-yaml-files-of-services/kube-mongo/experiment-2/docker_only/create_mongo_instance.sh
@@ -1,1 +1,1 @@
-docker run -p 27017:27017 --net=host --name mongo-worker-1 -v /data/db:/data/db -d mongo:3.2.4 --replSet rs0 --storageEngine wiredTiger --logappend --logpath /var/log/mongodb/mongod.log --bind_ip 0.0.0.0
+docker run -p 27017:27017 --net=host --name mongo-worker-$i -v /data/db:/data/db -d mongo:3.2.4 --replSet rs0 --storageEngine wiredTiger --logappend --logpath /var/log/mongodb/mongod.log --bind_ip 0.0.0.0
