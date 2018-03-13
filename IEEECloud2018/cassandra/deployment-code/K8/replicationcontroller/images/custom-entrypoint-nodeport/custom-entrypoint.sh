024d5fc522656186204ba4be22348fc6501caae8
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2018-03-13 22:41:04 +0000

    IEEE Cloud2018  research data and code

--- IEEECloud2018/cassandra/deployment-code/K8/replicationcontroller/images/custom-entrypoint-nodeport/custom-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sleep 5
-
-sed -i "s/native_transport_port: 9042/native_transport_port: $(echo ${CASSANDRA_NATIVE_PORT})/" /etc/cassandra/cassandra.yaml
-
-/docker-entrypoint.sh "$@"
--- IEEECloud2018/cassandra/deployment-code/K8/replicationcontroller/images/custom-entrypoint-seed-provider/custom-entrypoint-hostport.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sleep 5
-
-CASSANDRA_SEEDS=$CASSANDRA_SEED_DISCOVERY
-
-/docker-entrypoint.sh "$@"
